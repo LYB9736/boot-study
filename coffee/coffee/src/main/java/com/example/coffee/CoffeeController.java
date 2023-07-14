cf078c8e403a39599a4bd312930e97bd093eb87a
LYB9736 <chulfry1222@gmail.com>
2023-07-14 16:50:02 +0900

    coffee.html

---
+++ coffee/coffee/src/main/java/com/example/coffee/CoffeeController.java
@@ -0,0 +1,21 @@
+package com.example.coffee;
+
+import org.h2.engine.Mode;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class CoffeeController {
+
+    @GetMapping("/coffee")
+    public ModelAndView coffee(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("Americano", "아메리카노");
+        modelAndView.setViewName("coffee");
+
+        return modelAndView;
+    }
+    
+}
